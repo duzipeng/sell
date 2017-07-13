e2770d7df936655952517aebd1ea560319e65aa8
RegGear <june.dzp@qq.com>
2017-07-13 15:51:20 +0800

    商品详情页

--- src/components/food/food.vue
+++ src/components/food/food.vue
@@ -1,6 +1,6 @@
 <template>
   <transition name="move">
-    <div v-show="showFlag" class="food">
+    <div v-show="showFlag" class="food" ref="food">
       <div class="food-content">
         <div class="image-header">
           <img :src="food.image">
@@ -12,19 +12,28 @@
           <h1 class="title">{{food.name}}</h1>
           <div class="detail">
             <span class="sell-count">月售{{food.sellCount}}份</span>
-            <span class="rating">好评率{{food.rating}}</span>
+            <span class="rating">好评率{{food.rating}}%</span>
           </div>
           <div class="price">
-            <span class="now">￥{{food.price}}</span>
-            <span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
+            <span class="now">￥{{food.price}}</span><span class="old" v-show="food.oldPrice">￥{{food.oldPrice}}</span>
           </div>
         </div>
+        <div class="cartcontrol-wrapper">
+          <cart-control :food="food"></cart-control>
+        </div>
+        <transition>
+          <div class="buy" v-show="!food.count || food.count === 0" @click="addFirst">加入购物车</div>
+        </transition>
       </div>
     </div>
   </transition>
 </template>
 
 <script type="text/ecmascript-6">
+  import BScroll from 'better-scroll'
+  import Vue from 'vue'
+  import CartControl from 'components/cartcontrol/cartcontrol'
+
   export default {
     data() {
       return {
@@ -39,10 +48,30 @@
     methods: {
       show() {
         this.showFlag = true;
+        this.$nextTick(() => {
+          if (!this.scroll) {
+            this.scroll = new BScroll(this.$refs.food, {
+              click: true
+            });
+          } else {
+            this.scroll.refresh();
+          }
+        })
       },
       hide() {
         this.showFlag = false;
+      },
+      addFirst(event) {
+        if (!event._constructed) {
+          return;
+        }
+        console.log(event.target)
+        this.$emit('cartAdd', event.target);
+        Vue.set(this.food, 'count', 1)
       }
+    },
+    components: {
+      CartControl
     }
   }
 </script>
@@ -63,6 +92,7 @@
       transform: translate3d(100%, 0, 0)
     .image-header
       position: relative
+      width: 100%
       height: 0
       padding-top: 100%
       img
@@ -80,4 +110,54 @@
           padding: 10px
           font-size: 20px
           color: #fff
+    .content
+      padding: 18px
+      .title
+        line-height: 14px
+        margin-bottom: 8px
+        font-size: 14px
+        font-weight: 700
+        color: rgb(7, 17, 27)
+      .detail
+        margin-bottom: 18px
+        line-height: 10px
+        height: 10px
+        font-size: 0
+        .sell-count, .rating
+          font-size: 10px
+          color: rgb(147, 153, 159)
+        .sell-count
+          margin-right: 12px
+      .price
+        font-weight: 700px
+        line-height: 24px
+        .now
+          margin-right: 8px
+          font-size: 14px
+          color: rgb(240, 20, 20)
+        .old
+          text-decoration: line-through
+          font-size: 10px
+          color: rgb(147, 153, 159)
+    .cartcontrol-wrapper
+      position: absolute
+      right: 12px
+      bottom: 12px
+    .buy
+      position: absolute
+      right: 18px
+      bottom: 18px
+      z-index: 10
+      height: 24px
+      line-height: 24px
+      padding: 0 12px
+      box-sizing: border-box
+      border-radius: 12px
+      font-size: 10px
+      color: #fff
+      background: rgb(0, 160, 220)
+      &.fade-enter-active, &.fade-leave-active
+        transition: opacity  0.5s
+      &.fade-enter, &.fade-leave-to
+        opacity: 0
 </style>
